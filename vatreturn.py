29edeaa56d8e98c250b8185629f8fe85020a54ee
Seb Bacon <seb.bacon@gmail.com>
2019-04-02 09:16:02 +0100

    A dumb attempt to submit a return

--- vatreturn.py
+++ vatreturn.py
@@ -32,6 +32,15 @@ def index():
     ob = get_obligations().json()
     return "You have <pre>{ob}</pre> obligations on HMRC".format(ob=ob)
 
+@app.route("/submit")
+def submit():
+    if not hmrc.authorized:
+        return redirect(url_for("hmrc.login"))
+    else:
+        ob = set_returns().json()
+        return "You have returned thusly <pre>{ob}</pre>".format(ob=ob)
+
+
 # VAT endpoints
 # https://developer.service.hmrc.gov.uk/api-documentation/docs/api/service/vat-api/1.0
 def get_obligations():
@@ -40,6 +49,25 @@ def get_obligations():
     params = {'status': 'O'}  # open
     return hmrc.get(url, params=params)
 
+
+def set_returns():
+    data = {
+        "periodKey": "18A1",
+        "vatDueSales": 105.50,  # box 1 on the VAT Return form
+        "vatDueAcquisitions": -100.45, # box 2
+        "totalVatDue": 5.05, # box 3
+        "vatReclaimedCurrPeriod": 105.15,  # box 4
+        "netVatDue": 100.10,  # box 5
+        "totalValueSalesExVAT": 300, # box 6
+        "totalValuePurchasesExVAT": 300, # box7
+        "totalValueGoodsSuppliedExVAT": 3000, # box8
+        "totalAcquisitionsExVAT": 3000, # box 9
+        "finalised": True  # declaration
+    }
+    url = "/organisations/vat/393706722/returns"
+    return hmrc.post(
+        url,
+        data=data)
 
 def create_test_user():
     url = '/create-test-user/individuals'
