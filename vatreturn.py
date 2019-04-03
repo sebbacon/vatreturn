11362ed864a96230193d6650516a2f8fa1fda361
Seb Bacon <seb.bacon@gmail.com>
2019-04-03 12:38:11 +0100

    Ability to send a return

--- vatreturn.py
+++ vatreturn.py
@@ -1,16 +1,21 @@
 import json
 import os
 import requests
+import datetime
 
 from flask import Flask, redirect, url_for
 from flask import render_template, g
 from hmrc_provider import make_hmrc_blueprint, hmrc
+
+import pandas as pd
+
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
 app.config["HMRC_OAUTH_CLIENT_ID"] = os.environ.get("HMRC_OAUTH_CLIENT_ID")
 app.config["HMRC_OAUTH_CLIENT_SECRET"] = os.environ.get("HMRC_OAUTH_CLIENT_SECRET")
 app.config["VAT_NUMBER"] = os.environ.get("VAT_NUMBER")
+app.config["VAT_CSV"] = os.environ.get("VAT_CSV")
 hmrc_bp = make_hmrc_blueprint(
     scope='read:vat write:vat hello',
     client_id=app.config["HMRC_OAUTH_CLIENT_ID"],
@@ -39,12 +44,13 @@ def index():
     else:
         return redirect(url_for("obligations"))
 
+
 @app.route("/submit")
-def submit():
+def submit(period_key=None):
     if not hmrc.authorized:
         return redirect(url_for("hmrc.login"))
     else:
-        ob = set_returns().json()
+        ob = set_returns(period_key).json()
 
         return "You have returned thusly <pre>{ob}</pre>".format(ob=ob)
 
@@ -64,9 +70,17 @@ def get_endpoint(endpoint, params={}):
 
 # VAT endpoints
 @app.route("/obligations")
-def obligations():
-    params = {'status': 'O'}  # open
-
+def obligations(show_all=False):
+    if show_all:
+        today = datetime.date.today()
+        from_date = today - datetime.timedelta(days=365*2)
+        to_date = today
+        params = {
+            'from': from_date.strftime("%Y-%m-%d"),
+            'to': to_date.strftime("%Y-%m-%d")
+        }
+    else:
+        params = {'status': 'O'}
     obligations = get_endpoint('obligations', params)
     if 'error' in obligations:
         g.error = obligations['error']
@@ -75,31 +89,55 @@ def obligations():
     return render_template('obligations.html')
 
 
-
-def set_returns():
+def return_data(period_key):
+    df = pd.read_csv(app.config["VAT_CSV"])
+    assert list(df.columns) == ['VAT period', 'SUM of Fee', 'SUM of VAT']
+    period = df[df['VAT period'] == period_key]
+    net_fee = int(period['SUM of Fee'].iloc[0])
+    vat = int(period['SUM of VAT'].iloc[0])
+    gross_receipts = net_fee + vat
+    vat_due = gross_receipts * 0.165
+    box_1 = vat_due
+    box_2 = 0  # vat due on acquisitions
+    box_3 = box_1 + box_2  # total vat due - calculated: Box1 + Box2
+    box_4 = 0  # vat reclaimed for current period
+    box_5 = abs(box_3 - box_4)  # net vat due - calculate - amount to be paid. Take the figures from Box 3 and Box 4. Deduct the smaller figure from the larger one and put the difference
+    box_6 = gross_receipts # total value sales ex vat
+    box_7 = 0 # total value purchases ex vat
+    box_8 = 0 # total value goods supplied ex vat
+    box_9 = 0 # total acquisitions ex vat
     data = {
-        "periodKey": "18A1",
-        "vatDueSales": 105.50,  # box 1 on the VAT Return form
-        "vatDueAcquisitions": -100.45, # box 2
-        "totalVatDue": 5.05, # box 3
-        "vatReclaimedCurrPeriod": 105.15,  # box 4
-        "netVatDue": 100.10,  # box 5
-        "totalValueSalesExVAT": 300, # box 6
-        "totalValuePurchasesExVAT": 300, # box7
-        "totalValueGoodsSuppliedExVAT": 3000, # box8
-        "totalAcquisitionsExVAT": 3000, # box 9
+        "periodKey": period_key,
+        "vatDueSales": box_1,
+        "vatDueAcquisitions": box_2,
+        "totalVatDue": box_3,
+        "vatReclaimedCurrPeriod": box_4,
+        "netVatDue": box_5,
+        "totalValueSalesExVAT": box_6,
+        "totalValuePurchasesExVAT": box_7,
+        "totalValueGoodsSuppliedExVAT": box_8,
+        "totalAcquisitionsExVAT": box_9,
         "finalised": True  # declaration
     }
-    url = "/organisations/vat/393706722/returns"
-    asd = hmrc.post(
+    return data
+
+
+@app.route("/<string:period_key>/preview")
+def preview_return(period_key):
+    g.data = return_data(period_key)
+    return render_template('preview_return.html')
+
+
+@app.route("/<string:period_key>/send", methods=('POST',))
+def send_return(period_key):
+    data = return_data(period_key)
+    url = "/organisations/vat/{}/returns".format(app.config["VAT_NUMBER"])
+    response = hmrc.post(
         url,
         data=data)
-    import pdb; pdb.set_trace()
+    g.response = response.json()
+    return render_template('send_return.html')
 
-    return asd
-
-
-# other imports as necessary
 
 @app.route("/logout")
 def logout():
